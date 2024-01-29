d560d99f896275874bd9fd794d0537a877556827
Pepega42 <144796765+Pepega42@users.noreply.github.com>
2024-01-29 08:39:45 +0300

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using test;
+
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            Console.Write("Как вас зовут: ");
+            string UName = Console.ReadLine();
+            Test.StartTest(UName);
+            Console.WriteLine("Нажмите Enter, чтобы перезапустить тест");
+        } while (Console.ReadKey().Key == ConsoleKey.Enter);
+    }
+}
---
+++ test/Statistica.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public static class Statistics
+    {
+        private const string FileName = "fiel.json";
+        private static List<Usr> static1 = new List<Usr>();
+        static Statistics()
+        {
+            if (File.Exists(FileName))
+            {
+                var json = File.ReadAllText(FileName);
+                static1 = JsonConvert.DeserializeObject<List<Usr>>(json);
+            }
+        }
+        public static void AddStatistics(Usr userData)
+        {
+            static1.Add(userData);
+            SaveStatistics();
+        }
+        public static void Stisticboards()
+        {
+            Console.WriteLine("Статистика:");
+            Console.WriteLine("Имя    слов в минуту   слов в секунду");
+
+            foreach (var user in static1.OrderByDescending(s => s.Minutes))
+            {
+                Console.WriteLine($"{user.Minutes} {user.Seconds} {user.Name}");
+            }
+        }
+        private static void SaveStatistics()
+        {
+            var json = JsonConvert.SerializeObject(static1, Formatting.Indented);
+            File.WriteAllText(FileName, json);
+        }
+    }
+}
